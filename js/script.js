980dfaa20aa3da42577931cffe29e9ca16d6057b
KeithDelman <142185213+KeithDelman@users.noreply.github.com>
2023-12-15 01:04:15 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Swiper
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
+
+
+// menu open close
+
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu")
+}
+
+// Closs Menu On Scroll
+
+window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu")
+}
+
+// ScrollReveal Animation
+
+const animate = ScrollReveal ({
+    origin: "top",
+    distance: "60px",
+    duration: "2500",
+    delay: "100",
+})
+
+
+animate.reveal(".navbar");
+animate.reveal(".home-text",{origin: "left"});
+animate.reveal(".home-img",{origin: "bottom"});
+animate.reveal(".ser-box, .product-box, .team-box,.book-data",{interval: 100});
